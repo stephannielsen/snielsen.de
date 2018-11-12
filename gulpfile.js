d71c342e50627df5e43b533cbe3ff3b1d8418d90
Stephan Nielsen <stephannielsen@gmail.com>
2018-11-12 01:08:30 +0100

    Added medium claps.

--- gulpfile.js
+++ gulpfile.js
@@ -11,102 +11,106 @@ var serve = require("gulp-serve");
 
 // Copy third party libraries from /node_modules into /vendor
 gulp.task("vendor", function(done) {
-  // Bootstrap
-  gulp
-    .src([
-      "./node_modules/bootstrap/dist/**/*",
-      "!./node_modules/bootstrap/dist/css/bootstrap-grid*",
-      "!./node_modules/bootstrap/dist/css/bootstrap-reboot*"
-    ])
-    .pipe(gulp.dest("./vendor/bootstrap"));
+    // Bootstrap
+    gulp
+        .src([
+            "./node_modules/bootstrap/dist/**/*",
+            "!./node_modules/bootstrap/dist/css/bootstrap-grid*",
+            "!./node_modules/bootstrap/dist/css/bootstrap-reboot*"
+        ])
+        .pipe(gulp.dest("./vendor/bootstrap"));
 
-  // Devicons
-  gulp
-    .src([
-      "./node_modules/devicons/**/*",
-      "!./node_modules/devicons/*.json",
-      "!./node_modules/devicons/*.md",
-      "!./node_modules/devicons/!PNG",
-      "!./node_modules/devicons/!PNG/**/*",
-      "!./node_modules/devicons/!SVG",
-      "!./node_modules/devicons/!SVG/**/*"
-    ])
-    .pipe(gulp.dest("./vendor/devicons"));
+    // Devicons
+    gulp
+        .src([
+            "./node_modules/devicons/**/*",
+            "!./node_modules/devicons/*.json",
+            "!./node_modules/devicons/*.md",
+            "!./node_modules/devicons/!PNG",
+            "!./node_modules/devicons/!PNG/**/*",
+            "!./node_modules/devicons/!SVG",
+            "!./node_modules/devicons/!SVG/**/*"
+        ])
+        .pipe(gulp.dest("./vendor/devicons"));
 
-  // Devicon
-  gulp
-    .src([
-      "./node_modules/devicon/**/*",
-      "!./node_modules/devicon/*.json",
-      "!./node_modules/devicon/*.md",
-      "!./node_modules/devicon/gulpfile.js",
-      "!./node_modules/devicon/LICENSE",
-      "!./node_modules/devicon/*.html",
-      "!./node_modules/devicon/icons/**/*.eps"
-    ])
-    .pipe(gulp.dest("./vendor/devicon"));
+    // Devicon
+    gulp
+        .src([
+            "./node_modules/devicon/**/*",
+            "!./node_modules/devicon/*.json",
+            "!./node_modules/devicon/*.md",
+            "!./node_modules/devicon/gulpfile.js",
+            "!./node_modules/devicon/LICENSE",
+            "!./node_modules/devicon/*.html",
+            "!./node_modules/devicon/icons/**/*.eps"
+        ])
+        .pipe(gulp.dest("./vendor/devicon"));
 
-  // Font Awesome
-  gulp
-    .src([
-      "./node_modules/font-awesome/**/*",
-      "!./node_modules/font-awesome/{less,less/*}",
-      "!./node_modules/font-awesome/{scss,scss/*}",
-      "!./node_modules/font-awesome/.*",
-      "!./node_modules/font-awesome/*.{txt,json,md}"
-    ])
-    .pipe(gulp.dest("./vendor/font-awesome"));
+    // Font Awesome
+    gulp
+        .src([
+            "./node_modules/font-awesome/**/*",
+            "!./node_modules/font-awesome/{less,less/*}",
+            "!./node_modules/font-awesome/{scss,scss/*}",
+            "!./node_modules/font-awesome/.*",
+            "!./node_modules/font-awesome/*.{txt,json,md}"
+        ])
+        .pipe(gulp.dest("./vendor/font-awesome"));
 
-  // jQuery
-  gulp
-    .src([
-      "./node_modules/jquery/dist/*",
-      "!./node_modules/jquery/dist/core.js"
-    ])
-    .pipe(gulp.dest("./vendor/jquery"));
+    // jQuery
+    gulp
+        .src([
+            "./node_modules/jquery/dist/*",
+            "!./node_modules/jquery/dist/core.js"
+        ])
+        .pipe(gulp.dest("./vendor/jquery"));
 
-  // jQuery Easing
-  gulp
-    .src(["./node_modules/jquery.easing/*.js"])
-    .pipe(gulp.dest("./vendor/jquery-easing"));
+    // jQuery Easing
+    gulp
+        .src(["./node_modules/jquery.easing/*.js"])
+        .pipe(gulp.dest("./vendor/jquery-easing"));
 
-  // Simple Line Icons
-  gulp
-    .src(["./node_modules/simple-line-icons/fonts/**"])
-    .pipe(gulp.dest("./vendor/simple-line-icons/fonts"));
+    // Simple Line Icons
+    gulp
+        .src(["./node_modules/simple-line-icons/fonts/**"])
+        .pipe(gulp.dest("./vendor/simple-line-icons/fonts"));
 
-  gulp
-    .src(["./node_modules/simple-line-icons/css/**"])
-    .pipe(gulp.dest("./vendor/simple-line-icons/css"));
-  done();
+    gulp
+        .src(["./node_modules/simple-line-icons/css/**"])
+        .pipe(gulp.dest("./vendor/simple-line-icons/css"));
+
+    gulp
+        .src("./node_modules/rss-parser/dist/rss-parser.min.js")
+        .pipe(gulp.dest("./vendor/rss-parser"));
+    done();
 });
 
 // Compile SCSS
 gulp.task("css:compile", function() {
-  return gulp
-    .src("./scss/**/*.scss")
-    .pipe(
-      sass
-        .sync({
-          outputStyle: "expanded"
-        })
-        .on("error", sass.logError)
-    )
-    .pipe(gulp.dest("./css"));
+    return gulp
+        .src("./scss/**/*.scss")
+        .pipe(
+            sass
+            .sync({
+                outputStyle: "expanded"
+            })
+            .on("error", sass.logError)
+        )
+        .pipe(gulp.dest("./css"));
 });
 
 // Minify CSS
 gulp.task("css:minify", gulp.series("css:compile", function() {
-  return gulp
-    .src(["./css/*.css", "!./css/*.min.css"])
-    .pipe(cleanCSS())
-    .pipe(
-      rename({
-        suffix: ".min"
-      })
-    )
-    .pipe(gulp.dest("./css"))
-    .pipe(browserSync.stream());
+    return gulp
+        .src(["./css/*.css", "!./css/*.min.css"])
+        .pipe(cleanCSS())
+        .pipe(
+            rename({
+                suffix: ".min"
+            })
+        )
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.stream());
 }));
 
 // CSS
@@ -114,16 +118,16 @@ gulp.task("css", gulp.series(["css:compile", "css:minify"]));
 
 // Minify JavaScript
 gulp.task("js:minify", function() {
-  return gulp
-    .src(["./js/*.js", "!./js/*.min.js"])
-    .pipe(uglify())
-    .pipe(
-      rename({
-        suffix: ".min"
-      })
-    )
-    .pipe(gulp.dest("./js"))
-    .pipe(browserSync.stream());
+    return gulp
+        .src(["./js/*.js", "!./js/*.min.js"])
+        .pipe(uglify())
+        .pipe(
+            rename({
+                suffix: ".min"
+            })
+        )
+        .pipe(gulp.dest("./js"))
+        .pipe(browserSync.stream());
 });
 
 // JS
@@ -134,81 +138,85 @@ gulp.task("default", gulp.series(["css", "js", "vendor"]));
 
 // Configure the browserSync task
 gulp.task("browserSync", function(done) {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  done();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    done();
 });
 
 // Dev task
 gulp.task("dev", gulp.series(["css", "js", "browserSync"], function(done) {
-  gulp.watch("./scss/*.scss", gulp.series("css"));
-  gulp.watch("./js/*.js", gulp.series("js"));
-  gulp.watch("./*.html", gulp.series(browserSync.reload));
-  done();
+    gulp.watch("./scss/*.scss", gulp.series("css"));
+    gulp.watch("./js/*.js", gulp.series("js"));
+    gulp.watch("./*.html", gulp.series(browserSync.reload));
+    done();
 }));
 
 //Clean dist folder
 gulp.task("clean:dist", function() {
-  return del(["dist/**/*"]);
+    return del(["dist/**/*"]);
 });
 
 // Dist task
 gulp.task("build:dist", gulp.series(["clean:dist", "default"], function(done) {
-  gulp.src(["./css/*.min.css"]).pipe(gulp.dest("./dist/css"));
-  gulp.src(["./js/*.min.js"]).pipe(gulp.dest("./dist/js"));
-  gulp.src(["./img/*"]).pipe(gulp.dest("./dist/img"));
-  gulp
-    .src(["./vendor/bootstrap/css/bootstrap.min.css"])
-    .pipe(gulp.dest("./dist/vendor/bootstrap/css"));
-  gulp
-    .src(["./vendor/bootstrap/js/bootstrap.bundle.min.js"])
-    .pipe(gulp.dest("./dist/vendor/bootstrap/js"));
-  gulp
-    .src(["./vendor/devicons/css/*.min.css"])
-    .pipe(gulp.dest("./dist/vendor/devicons/css"));
-  gulp
-    .src(["./vendor/devicons/fonts/*"])
-    .pipe(gulp.dest("./dist/vendor/devicons/fonts"));
-  gulp
-    .src([
-      "./vendor/devicon/devicon-colors.css",
-      "./vendor/devicon/devicon.min.css"
-    ])
-    .pipe(gulp.dest("./dist/vendor/devicon"));
-  gulp
-    .src(["./vendor/devicon/fonts/*"])
-    .pipe(gulp.dest("./dist/vendor/devicon/fonts"));
-  gulp
-    .src(["./vendor/font-awesome/css/*.min.css"])
-    .pipe(gulp.dest("./dist/vendor/font-awesome/css"));
-  gulp
-    .src(["./vendor/font-awesome/fonts/*"])
-    .pipe(gulp.dest("./dist/vendor/font-awesome/fonts"));
-  gulp
-    .src(["./vendor/jquery/jquery.min.js"])
-    .pipe(gulp.dest("./dist/vendor/jquery"));
-  gulp
-    .src(["./vendor/jquery-easing/*.min.js"])
-    .pipe(gulp.dest("./dist/vendor/jquery-easing"));
-  gulp
-    .src(["./vendor/simple-line-icons/css/simple-line-icons.css"])
-    .pipe(gulp.dest("./dist/vendor/simple-line-icons/css"));
-  gulp
-    .src(["./vendor/simple-line-icons/fonts/*"])
-    .pipe(gulp.dest("./dist/vendor/simple-line-icons/fonts"));
-  gulp.src([
-    "*.html",
-    "*.png",
-    "*.ico",
-    "*.svg",
-    "site.webmanifest",
-    "browserconfig.xml"
-  ]).pipe(gulp.dest("./dist"));
-  gulp.src(["mailer.php"]).pipe(gulp.dest("./dist"));
-  done();
+    gulp.src(["./css/*.min.css"]).pipe(gulp.dest("./dist/css"));
+    gulp.src(["./js/*.min.js"]).pipe(gulp.dest("./dist/js"));
+    gulp.src(["./img/*"]).pipe(gulp.dest("./dist/img"));
+    gulp
+        .src(["./vendor/bootstrap/css/bootstrap.min.css"])
+        .pipe(gulp.dest("./dist/vendor/bootstrap/css"));
+    gulp
+        .src(["./vendor/bootstrap/js/bootstrap.bundle.min.js"])
+        .pipe(gulp.dest("./dist/vendor/bootstrap/js"));
+    gulp
+        .src(["./vendor/devicons/css/*.min.css"])
+        .pipe(gulp.dest("./dist/vendor/devicons/css"));
+    gulp
+        .src(["./vendor/devicons/fonts/*"])
+        .pipe(gulp.dest("./dist/vendor/devicons/fonts"));
+    gulp
+        .src([
+            "./vendor/devicon/devicon-colors.css",
+            "./vendor/devicon/devicon.min.css"
+        ])
+        .pipe(gulp.dest("./dist/vendor/devicon"));
+    gulp
+        .src(["./vendor/devicon/fonts/*"])
+        .pipe(gulp.dest("./dist/vendor/devicon/fonts"));
+    gulp
+        .src(["./vendor/font-awesome/css/*.min.css"])
+        .pipe(gulp.dest("./dist/vendor/font-awesome/css"));
+    gulp
+        .src(["./vendor/font-awesome/fonts/*"])
+        .pipe(gulp.dest("./dist/vendor/font-awesome/fonts"));
+    gulp
+        .src(["./vendor/jquery/jquery.min.js"])
+        .pipe(gulp.dest("./dist/vendor/jquery"));
+
+    gulp
+        .src("./vendor/rss-parser/rss-parser.min.js")
+        .pipe(gulp.dest("./dist/vendor/rss-parser"));
+    gulp
+        .src(["./vendor/jquery-easing/*.min.js"])
+        .pipe(gulp.dest("./dist/vendor/jquery-easing"));
+    gulp
+        .src(["./vendor/simple-line-icons/css/simple-line-icons.css"])
+        .pipe(gulp.dest("./dist/vendor/simple-line-icons/css"));
+    gulp
+        .src(["./vendor/simple-line-icons/fonts/*"])
+        .pipe(gulp.dest("./dist/vendor/simple-line-icons/fonts"));
+    gulp.src([
+        "*.html",
+        "*.png",
+        "*.ico",
+        "*.svg",
+        "site.webmanifest",
+        "browserconfig.xml"
+    ]).pipe(gulp.dest("./dist"));
+    gulp.src(["mailer.php"]).pipe(gulp.dest("./dist"));
+    done();
 }));
 
 gulp.task("dist", gulp.series("build:dist", serve({ root: "dist", port: 3030 })));
--- js/resume.js
+++ js/resume.js
@@ -1,121 +1,129 @@
 (function($) {
-  "use strict"; // Start of use strict
+    "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      if (target.length) {
-        $("html, body").animate(
-          {
-            scrollTop: target.offset().top
-          },
-          1000,
-          "easeInOutExpo"
-        );
-        return false;
-      }
-    }
-  });
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate({
+                        scrollTop: target.offset().top
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $(".js-scroll-trigger").click(function() {
-    $(".navbar-collapse").collapse("hide");
-    $("#legalLinks").collapse("hide");
-  });
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function() {
+        $(".navbar-collapse").collapse("hide");
+        $("#legalLinks").collapse("hide");
+    });
 
-  $(".navbar-toggler").click(function() {
-    $("#legalLinks").collapse("toggle");
-  });
+    $(".navbar-toggler").click(function() {
+        $("#legalLinks").collapse("toggle");
+    });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $("body").scrollspy({
-    target: "#sideNav"
-  });
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#sideNav"
+    });
 })(jQuery); // End of use strict
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function() {
-  "use strict";
-  window.addEventListener(
-    "load",
-    function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName("needs-validation");
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener(
-          "submit",
-          function(event) {
-            if (form.checkValidity() === false) {
-              event.preventDefault();
-              event.stopPropagation();
-            }
-            form.classList.add("was-validated");
-          },
-          false
-        );
-      });
-    },
-    false
-  );
+    "use strict";
+    window.addEventListener(
+        "load",
+        function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName("needs-validation");
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener(
+                    "submit",
+                    function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add("was-validated");
+                    },
+                    false
+                );
+            });
+        },
+        false
+    );
 })();
 
 (function() {
-  "use strict";
-  // Get the form.
-  var form = $("#contact-form");
+    "use strict";
 
-  // Get the messages div.
-  var formMessages = $("#form-messages");
+    new MediumClaps({
+        feedUrl: 'https://allorigins.me/get?url=' + encodeURIComponent('https://medium.com/feed/@stephannielsen/has-recommended'),
+        missingImage: 'https://source.unsplash.com/random/640x480',
+    });
+})();
 
-  // Set up an event listener for the contact form.
-  $(form).submit(function(event) {
-    // Stop the browser from submitting the form.
-    event.preventDefault();
-    $(formMessages).css("visibility", "hidden");
+(function() {
+    "use strict";
+    // Get the form.
+    var form = $("#contact-form");
 
-    var formData = $(form).serialize();
-    // Submit the form using AJAX.
-    $.ajax({
-      type: "POST",
-      url: $(form).attr("action"),
-      data: formData
-    })
-      .done(function(response) {
-        // Make sure that the formMessages div has the 'success' class.
-        $(formMessages).removeClass("alert-danger");
-        $(formMessages).addClass("alert-success");
+    // Get the messages div.
+    var formMessages = $("#form-messages");
 
-        // Set the message text.
-        $(formMessages).text(response);
-        $(formMessages).css("visibility", "visible");
+    // Set up an event listener for the contact form.
+    $(form).submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+        $(formMessages).css("visibility", "hidden");
 
-        // Clear the form.
-        $("#nameInput").val("");
-        $("#emailInput").val("");
-        $("#messageInput").val("");
-        $(form).removeClass("was-validated");
-      })
-      .fail(function(data) {
-        // Make sure that the formMessages div has the 'error' class.
-        $(formMessages).removeClass("alert-success");
-        $(formMessages).addClass("alert-danger");
+        var formData = $(form).serialize();
+        // Submit the form using AJAX.
+        $.ajax({
+                type: "POST",
+                url: $(form).attr("action"),
+                data: formData
+            })
+            .done(function(response) {
+                // Make sure that the formMessages div has the 'success' class.
+                $(formMessages).removeClass("alert-danger");
+                $(formMessages).addClass("alert-success");
 
-        // Set the message text.
-        if (data.responseText !== "") {
-          $(formMessages).text(data.responseText);
-        } else {
-          $(formMessages).text(
-            "Oops! An error occured and your message could not be sent."
-          );
-        }
-        $(formMessages).css("visibility", "visible");
-      });
-  });
+                // Set the message text.
+                $(formMessages).text(response);
+                $(formMessages).css("visibility", "visible");
+
+                // Clear the form.
+                $("#nameInput").val("");
+                $("#emailInput").val("");
+                $("#messageInput").val("");
+                $(form).removeClass("was-validated");
+            })
+            .fail(function(data) {
+                // Make sure that the formMessages div has the 'error' class.
+                $(formMessages).removeClass("alert-success");
+                $(formMessages).addClass("alert-danger");
+
+                // Set the message text.
+                if (data.responseText !== "") {
+                    $(formMessages).text(data.responseText);
+                } else {
+                    $(formMessages).text(
+                        "Oops! An error occured and your message could not be sent."
+                    );
+                }
+                $(formMessages).css("visibility", "visible");
+            });
+    });
 })();
