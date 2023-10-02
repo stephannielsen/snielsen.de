5035e4217bd409df87cbde085ca36d4b482918ec
Stephan Nielsen <stephannielsen@gmail.com>
2023-10-02 10:15:10 +0200

    chore: Removed some outdated parts

--- js/resume.js
+++ js/resume.js
@@ -3,7 +3,6 @@ import 'jquery.easing'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
 import '@fortawesome/fontawesome-free/css/all.css'
-import { MediumClaps } from './medium-claps.js'
 import 'devicons/css/devicons.min.css'
 import './../scss/resume.scss'
 
@@ -46,93 +45,3 @@ import './../scss/resume.scss'
         target: "#sideNav"
     });
 })($); // End of use strict
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-    "use strict";
-    window.addEventListener(
-        "load",
-        function() {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName("needs-validation");
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function(form) {
-                form.addEventListener(
-                    "submit",
-                    function(event) {
-                        if (form.checkValidity() === false) {
-                            event.preventDefault();
-                            event.stopPropagation();
-                        }
-                        form.classList.add("was-validated");
-                    },
-                    false
-                );
-            });
-        },
-        false
-    );
-})();
-
-(function() {
-    "use strict";
-
-    new MediumClaps({
-        feedUrl: 'https://allorigins.snielsen.de/get?url=' + encodeURIComponent('https://medium.com/feed/@stephannielsen/has-recommended'),
-        missingImage: 'https://source.unsplash.com/random/640x480',
-    });
-})();
-
-(function() {
-    "use strict";
-    // Get the form.
-    var form = $("#contact-form");
-
-    // Get the messages div.
-    var formMessages = $("#form-messages");
-
-    // Set up an event listener for the contact form.
-    $(form).submit(function(event) {
-        // Stop the browser from submitting the form.
-        event.preventDefault();
-        $(formMessages).css("visibility", "hidden");
-
-        var formData = $(form).serialize();
-        // Submit the form using AJAX.
-        $.ajax({
-                type: "POST",
-                url: $(form).attr("action"),
-                data: formData
-            })
-            .done(function(response) {
-                // Make sure that the formMessages div has the 'success' class.
-                $(formMessages).removeClass("alert-danger");
-                $(formMessages).addClass("alert-success");
-
-                // Set the message text.
-                $(formMessages).text(response);
-                $(formMessages).css("visibility", "visible");
-
-                // Clear the form.
-                $("#nameInput").val("");
-                $("#emailInput").val("");
-                $("#messageInput").val("");
-                $(form).removeClass("was-validated");
-            })
-            .fail(function(data) {
-                // Make sure that the formMessages div has the 'error' class.
-                $(formMessages).removeClass("alert-success");
-                $(formMessages).addClass("alert-danger");
-
-                // Set the message text.
-                if (data.responseText !== "") {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text(
-                        "Oops! An error occured and your message could not be sent."
-                    );
-                }
-                $(formMessages).css("visibility", "visible");
-            });
-    });
-})();
